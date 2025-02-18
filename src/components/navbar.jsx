1e40cdb233332efc7f54a46c5ac648d3b532fdfe
Lyu.Hiroyama <lyuhiroyama@gmail.com>
2025-02-19 07:24:51 +0900

    feat: メニューバーへのデザイン追加

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 export default function Navbar(){
     return (
         <nav className="nav">
-            <Link to="/" className="site-title">廣山 龍(ひろやま りゅう)</Link>
+            <Link to="/" className="site-title">Ryu Hiroyama のポートフォリオ</Link>
             <ul>
                 <li>
                     <Link to="/projects">Projects</Link>
