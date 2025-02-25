e7d7a564976cf9eb4f856751339f8493d276d125
lyuhiroyama <lyuhiroyama@gmail.com>
2025-02-25 12:36:53 +0900

    feat: 各業務例リストに使用言語を追加

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -6,19 +6,23 @@ export default function About(){
         {
             title: "株式会社 PLAY&co",
             titleDate: "Oct 2023 - Now",
-            description: "ホステル運営会社のシステム部署にてアルバイトとして勤務 ",
+            isAnimated: true,
+            description: "ホステル運営会社のシステム部署にて勤務 ",
             subSecTitle: "業務例",
             keyList: [
-                "GASとSlack APIを活用し、自動議事録生成 & Slack通知ボットの開発",
-                "GASを活用した定期清掃済み部屋・館内エリアの追跡ツールの作成",
-                "スプレッドシートの数式・コードを活用し、財務管理に使用するツールの開発",
-                "ユーザーからのCSVアップロードを受信・追記・保存するGASスクリプトを作成 (文字化けエラーに対応機能の実装を含む)"
+                "自動議事録生成 & Slack通知ボットの開発",
+                "清掃済み部屋・共用エリアの追跡ツール作成",
+                "CSVアップロードの受信・追記・保存スクリプト作成",
+                "末チェックインゲストのデータをシステムから抽出",
+                "現場でのフロント・清掃業務にも従事"
             ],
-            techList: ["Google Apps Script", "JavaScript", "HTML", "CSS", "Python"]
+            keyListLang: ["(GAS・Slack API)", "(GAS)", "(GAS)", "(Python・Selenium)"],
+            techList: ["Google Apps Script", "JavaScript", "HTML", "CSS", "Python", "Selenium"]
         },
         {
             title: "University of the Fraser Valley",
             titleDate: "Jan 2022 - June 2023",
+            isAnimated: false,
             description: "Computer Information Systems certificate プログラムを卒業",
             subSecTitle: "修得授業・課外活動",
             keyList: [
@@ -31,6 +35,7 @@ export default function About(){
                 "統計学入門",
                 "Computing Students Association の代表委員"
             ],
+            keyListLang: [],
             techList: ["Assembly", "JavaScript", "jQuery", "HTML", "CSS", "Python"]
         }
     ]
@@ -38,7 +43,8 @@ export default function About(){
     return (
         <div className="about-component">
             <div className="about-container">
-                <div>Relevant Experience</div>
+                <div style={{fontSize: "1.3rem"}}>Relevant Experience</div>
+
                 {experienceData.map((experience, index) => (
                     <ExperienceCard key={index} {...experience} />
                 ))}
--- src/components/experienceCard.jsx
+++ src/components/experienceCard.jsx
@@ -1,13 +1,13 @@
 import { Fragment } from 'react'; // <> </> に属性を追加するため
 
 export default function ExperienceCard(
-    {title, titleDate, description, subSecTitle, keyList, techList}
+    {title, titleDate, isAnimated, description, subSecTitle, keyList, keyListLang, techList}
 ) {
 
     return (
         <div className="experience-component">
             <div className="timeline-marker">
-                <div className="timeline-circle"></div>
+                <div className={`timeline-circle ${isAnimated ? 'is-animated' : ''}`}></div>
                 <div className="timeline-line"></div>
             </div>
             <div className="experience-div">
@@ -17,9 +17,12 @@ export default function ExperienceCard(
                 </div>
                 <div className="main-description">{description}</div>
                 <div className="sub-section-title">{subSecTitle}</div>
-                <ul className="key-list">
-                    {keyList.map(key => (
-                        <li>{key}</li>
+                <ul className="key-list-ul">
+                    {keyList.map((key, index) => (
+                        <li>
+                            <div className='key-list-line'>{key}</div>
+                            <div>{keyListLang[index]}</div>
+                        </li>
                     ))}
                 </ul>
                 <div className="tech-list">
