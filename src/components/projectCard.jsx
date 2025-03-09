88434be0885b96550061921a2630fbdd6ae23acc
lyuhiroyama <lyuhiroyama@gmail.com>
2025-03-09 09:47:36 +0900

    feat: upvote機能をクライアント側で実装

--- src/components/projectCard.jsx
+++ src/components/projectCard.jsx
@@ -1,14 +1,37 @@
+import { useState } from 'react';
 import { Fragment } from 'react'; // <> </> に属性を追加するため
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'; // Font Awesome 使用のため
 import { faGithub } from '@fortawesome/free-brands-svg-icons'; // Font Awesome GitHub icon
+import { faCaretUp } from '@fortawesome/free-solid-svg-icons'
+
 
 export default function ProjectCard({
-        projectTitle, thumbnail, technologies, description, buttonText, gitHubLink, projectLink
+        projectTitle, thumbnail, technologies, description, buttonText, gitHubLink, projectLink, votes
     }){
+
+    const [voteCount, setVoteCount] = useState(votes);
+    const [hasVoted, setHasVoted] = useState(false);
+
+    const handleVote = async () => {
+        if (hasVoted) return;
+
+        setVoteCount(prev => prev + 1);
+        setHasVoted(true);
+    }
 
     return (
         <div className="projects-component">
 
+            <div className='votes-container'>
+                <button
+                    onClick={handleVote}
+                    disabled={hasVoted}
+                    className={`vote-button ${hasVoted ? 'voted' : ''}`}
+                >
+                    <FontAwesomeIcon icon={faCaretUp} />
+                </button>
+                <div className='vote-count'>{voteCount}</div>
+            </div>
             <div className="project-thumbnail-container">
                 <a href={projectLink}>
                     <img src={thumbnail} alt={projectTitle} />
