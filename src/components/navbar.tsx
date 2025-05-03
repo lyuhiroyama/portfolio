f0329e06d78d6262efaf59bedd3348c0251840fc
lyuhiroyama <lyuhiroyama@gmail.com>
2025-05-03 14:26:46 +0900

    feat: Add globe button for language toggling
    - Did some refactoring of class names as well

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,23 +1,28 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { faBars, faGlobe } from "@fortawesome/free-solid-svg-icons";
 import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
 import NavOverlay from "./navOverlay";
 
 export default function Navbar(): React.ReactElement {
     const [isNavActive, setIsNavActive] = useState(false);
+    const [isLangDropdownVisible, setIsLangDropdownVisible] = useState(false);
+
     const handleNavClick = () => {
         window.scrollTo(0, 0);
     };
     const handleOverlayClick = () => {
         setIsNavActive(false);
     };
+    const toggleLangDropdown = () => {
+        setIsLangDropdownVisible(!isLangDropdownVisible);
+    };
 
     return (
         <div className="nav-component">
             <nav className="navbar">
-                <Link to="/" className="site-title" onClick={handleNavClick}>
+                <Link to="/" className="site-title nav-buttons" onClick={handleNavClick}>
                     Ryu Hiroyama
                 </Link>
 
@@ -30,28 +35,47 @@ export default function Navbar(): React.ReactElement {
 
                 <ul>
                     <li>
-                        <Link to="/" onClick={handleNavClick}>
+                        <Link to="/" onClick={handleNavClick} className="nav-buttons">
                             Home
                         </Link>
                     </li>
                     <li>
-                        <Link to="/about">About</Link>
+                        <Link to="/about" className="nav-buttons">About</Link>
                     </li>
                     <li>
-                        <a href="https://github.com/lyuhiroyama">
+                        <a href="https://github.com/lyuhiroyama" className="nav-buttons">
                             <FontAwesomeIcon
-                                className="home-icons"
                                 icon={faGithub}
                             />
                         </a>
                     </li>
                     <li>
-                        <a href="https://www.linkedin.com/in/ryu-h-168436157/">
-                            <FontAwesomeIcon
-                                className="home-icons"
-                                icon={faLinkedin}
-                            />
+                        <a href="https://www.linkedin.com/in/ryu-h-168436157/" className="nav-buttons">
+                            <FontAwesomeIcon icon={faLinkedin} />
                         </a>
+                    </li>
+                    <li>
+                        <button onClick={toggleLangDropdown} className="nav-buttons">
+                            <FontAwesomeIcon icon={faGlobe} />
+                        </button>
+                        {isLangDropdownVisible && (
+                            <ul className="language-dropdown">
+                                <li
+                                    onClick={() =>
+                                        console.log("Switch to English")
+                                    }
+                                >
+                                    English
+                                </li>
+                                <li
+                                    onClick={() =>
+                                        console.log("Switch to Japanese")
+                                    }
+                                >
+                                    日本語
+                                </li>
+                            </ul>
+                        )}
                     </li>
                 </ul>
             </nav>
