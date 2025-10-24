1c18800a944c9ff566ce29a804bd1d12175913f7
lyuhiroyama <lyuhiroyama@gmail.com>
2025-10-24 12:43:01 +0900

    feat(frontend): Default language to detected user browser configured language

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,17 +1,18 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
+import languageDetector from 'i18next-browser-languagedetector';
 
 import en from './locales/en.json';
 import ja from './locales/ja.json';
 
 i18n
+  .use(languageDetector)
   .use(initReactI18next)
   .init({
     resources: {
       en: { translation: en },
       ja: { translation: ja },
     },
-    lng: 'en',
     fallbackLng: 'en',
     interpolation: { escapeValue: false },
   });
